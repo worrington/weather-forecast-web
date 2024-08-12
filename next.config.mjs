892aae9b621b211d50435640a815c4333ff8c0d6
Montserrat <mdanatg@gmail.com>
2024-08-12 07:09:12 -0600

    fix: new deplor

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,11 @@ const nextConfig = {
   },
   assetPrefix: process.env.NODE_ENV === 'production' ? '/weather-forecast-web/' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/weather-forecast-web' : '',
+  env: {
+    NEXT_PUBLIC_RESERVAMOS_API: process.env.NEXT_PUBLIC_RESERVAMOS_API,
+    NEXT_PUBLIC_OPEN_WEATHER_API: process.env.NEXT_PUBLIC_OPEN_WEATHER_API,
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
 };
 
 export default nextConfig;
