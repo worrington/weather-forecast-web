f116fd22b198c0a9fbeadbe924b66c01a58cb053
Montserrat <mdanatg@gmail.com>
2024-08-12 07:38:12 -0600

    Deploy to GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,6 @@ const nextConfig = {
       },
     ],
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/weather-forecast-web/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/weather-forecast-web' : '',
   env: {
     NEXT_PUBLIC_RESERVAMOS_API: process.env.NEXT_PUBLIC_RESERVAMOS_API,
     NEXT_PUBLIC_OPEN_WEATHER_API: process.env.NEXT_PUBLIC_OPEN_WEATHER_API,
